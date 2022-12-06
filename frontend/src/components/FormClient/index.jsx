37fdd1a303623061ebcf847fe462161015024b18
GabrielleMedeirosOliveira <gabriellemedeiros51@gmail.com>
2022-12-06 18:35:47 -0300

    feat: Adicionado Toaster e limpeza de formulário pos envio de dados

--- frontend/src/components/FormClient/index.jsx
+++ frontend/src/components/FormClient/index.jsx
@@ -4,43 +4,77 @@ import * as yup from "yup";
 import { useContext } from "react";
 import style from "./style.module.css";
 import { ClientDataContext } from "../../context/clientData";
+import toast, { Toaster } from 'react-hot-toast';
+import { useState } from "react";
 
 export const FormClient = () => {
   const { getClientData, getAllClients } = useContext(ClientDataContext);
+  const [ name, setName ] = useState('');
+  const [ email, setEmail] = useState('');
+  const [ fone, setFone] = useState("");
+
+  const notify = () => toast.success('Cliente Cadastrado com Sucesso');
 
   const formSchema = yup.object().shape({
-    name: yup.string().required("Field Required!"),
-    email: yup.string().required("Field Required!"),
-    fone: yup.string().required("Field Required!"),
+    name: yup.string().required("Campo Obrigatório!"),
+    email: yup.string().required("Campo Obrigatório!"),
+    fone: yup.string().required("Campo Obrigatório!"),
   });
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, formState:{errors}, } = useForm({
     resolver: yupResolver(formSchema),
   });
+
+  const clearForm = () => {
+    setName('')
+    setEmail('')
+    setFone('')
+  }
 
   const onSubmit = (data) => {
     getClientData(data)
     getAllClients()
+    notify()
+    clearForm()
   };
 
   return (
     <>
+    <Toaster />
       <form onSubmit={handleSubmit(onSubmit)} className={style.container}>
         <label className={style.inputLabel}>Nome:</label>
-          <input type="text" name="name" {...register("name")} className={style.input} />
-        
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome Completo"
+            {...register("name")}
+            className={style.input}
+            value={name}
+            onChange={ev => setName(ev.target.value)}
+          />
+          {<span>{errors.name?.message}</span>}
         <label className={style.inputLabel}>Email:</label>
-          <input type="email" name="email" {...register("email")} className={style.input} />
-        
+          <input 
+            type="email"
+            name="email" 
+            placeholder="email@email.com"
+            {...register("email")} 
+            className={style.input} 
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+          />
+          {<span>{errors.email?.message}</span>}
         <label className={style.inputLabel}>Telefone:</label>
           <input
-          type="text"
-          name="fone"
-          placeholder="(11)94002-8922"
-          {...register("fone")}
-          className={style.input}
+            type="text"
+            name="fone"
+            placeholder="(11)94002-8922"
+            {...register("fone")}
+            className={style.input}
+            value={fone}
+            onChange={ev => setFone(ev.target.value)}
           />
-
-        <button type="submit" className={style.btn_register_client}>Cadastrar</button>
+          {<span>{errors.fone?.message}</span>}
+        <button type="submit" className={style.btn_register_client}>Cadastrar Cliente</button>
       </form>
     </>
   );
--- frontend/src/components/FormContact/index.jsx
+++ frontend/src/components/FormContact/index.jsx
@@ -1,24 +1,41 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { useContext, useEffect} from "react";
+import { useContext, useEffect, useState} from "react";
 import { ContactDataContext } from "../../context/contactData";
 import style from "./style.module.css";
 import { ClientDataContext } from "../../context/clientData";
+import toast, { Toaster } from 'react-hot-toast';
+
 export const FormContact = () => {
   const { getContactData } = useContext(ContactDataContext);
   const { getAllClients, clients } = useContext(ClientDataContext);
+  const [ name, setName ] = useState('');
+  const [ email, setEmail] = useState('');
+  const [ fone, setFone] = useState("");
+
+  const notify = () => toast.success('Contato Cadastrado com Sucesso');
+
   const formSchema = yup.object().shape({
-    name: yup.string().required("Field Required!"),
-    email: yup.string().required("Field Required!"),
-    fone: yup.string().required("Field Required!"),
-    client_id: yup.string().required("Field Required!"),
+    name: yup.string().required("Campo Obrigatório!"),
+    email: yup.string().required("Campo Obrigatório!"),
+    fone: yup.string().required("Campo Obrigatório!"),
+    client_id: yup.string().required("Campo Obrigatório!"),
   });
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, formState: { errors }, } = useForm({
     resolver: yupResolver(formSchema),
   });
+
+  const clearForm = () => {
+    setName('')
+    setEmail('')
+    setFone('')
+  }
+
   const onSubmit = (data) => {
     getContactData(data);
+    notify();
+    clearForm()
   };
 
   useEffect(() => {
@@ -27,11 +44,30 @@ export const FormContact = () => {
   }, []);
   return (
     <>
+     <Toaster />
       <form onSubmit={handleSubmit(onSubmit)} className={style.container}>
         <label className={style.inputLabel}>Nome:</label>
-        <input type="text" name="name" {...register("name")} className={style.input} />
+        <input 
+          type="text" 
+          name="name" 
+          placeholder="Nome Completo"
+          {...register("name")} 
+          className={style.input}
+          value={name}
+          onChange={ev => setName(ev.target.value)}
+        />
+        {<span>{errors.name?.message}</span>}
         <label className={style.inputLabel}>Email:</label>
-        <input type="email" name="email" {...register("email")} className={style.input} />
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="email@email.com"
+          {...register("email")} 
+          className={style.input} 
+          value={email}
+          onChange={ev => setEmail(ev.target.value)}
+        />
+        {<span>{errors.email?.message}</span>}
         <label className={style.inputLabel}>Telefone:</label>
         <input
           type="text"
@@ -39,7 +75,10 @@ export const FormContact = () => {
           placeholder="(11)94002-8922"
           {...register("fone")}
           className={style.input}
+          value={fone}
+          onChange={ev => setFone(ev.target.value)}
         />
+        {<span>{errors.fone?.message}</span>}
         <label className={style.inputLabel}>Selecione Cliente:</label>
         <select name="client" id="client" {...register("client_id")} className={style.input}>
           {clients?.map((client) => {
@@ -50,7 +89,7 @@ export const FormContact = () => {
             );
           })}
         </select>
-        <button type="submit" className={style.btn_register_contact}>Cadastrar</button>
+        <button type="submit" className={style.btn_register_contact}>Cadastrar Contato</button>
       </form>
     </>
   );
